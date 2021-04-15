9157e2c7e04d1047967fd79b3bce0ea8c474becd
ZhangJi <zhangji@hotmail.com>
2021-04-15 17:05:01 +0800

    test

--- src/hook/TestRef2.js
+++ src/hook/TestRef2.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function TestRef2(props) {
+  const inputRef = React.useRef(null);
 
-    const inputRef = React.useRef(null);
+  React.useEffect(() => {
+    console.log(
+      inputRef.current.value,
+      inputRef.current.type,
+      inputRef.current.data
+    );
+    console.log(inputRef, inputRef.current);
+    inputRef.current.focus();
+    inputRef.current.value = "abc";
+  });
 
-    React.useEffect(() => {
-        console.log( inputRef.current.value, inputRef.current.type );
-        console.log(inputRef, inputRef.current );
-        inputRef.current.focus();
-        inputRef.current.value = "abc";
-    });
-
-    return (
-        <input type="text" value="ZhangSan" ref={inputRef} />
-    )
-
+  return (
+    <div>
+      <input type="text" value="LiSi" />
+      <input
+        type="text"
+        value="ZhangSan"
+        ref={inputRef}
+        data="dataRefValue"
+      />
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import Page1 from "./hook/Page1";
 import TestUseInterval from "./hook/TestUseInterval";
 import ContextWrapper from "./hook/ContextWrapper";
+import TestRef2 from "./hook/TestRef2";
 import TestColor from "./test1/TestColor";
 import Counter0 from "./f1/Counter0";
 
 function App() {
   return (
     <div>
-      <ContextWrapper />
+      <TestRef2 />
     </div>
   );
 }
